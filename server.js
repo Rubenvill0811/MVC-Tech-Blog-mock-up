6bb15b4999f6a282cac286670e4a6b4616a262fa
Rubenvill0811 <rubenvill0811@gmail.com>
2024-07-16 11:01:57 -0700

    Typo scan and correction

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require('express');
-const path = require('path');
-const session = require('express-session');
-const handlebars = require('express-handlebars');
-const routes = require('./controllers');
-const sequelize = require('./config');
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const handlebars = require("express-handlebars");
+const routes = require("./controllers");
+const sequelize = require("./config");
 const PORT = process.env.PORT || 3001;
-const cookieStorage = require('connect-session-sequelize')(session.Store);
+const cookieStorage = require("connect-session-sequelize")(session.Store);
 const app = express();
 const handlebarsConnection = handlebars.create();
 
 const sesh = {
-    secret: 'hello',
-    cookie: {
-        maxAge: 100000
-    },
-    store: new cookieStorage({db:sequelize})
-}
+  secret: "hello",
+  cookie: {
+    maxAge: 100000,
+  },
+  store: new cookieStorage({ db: sequelize }),
+};
 app.engine("handlebars", handlebarsConnection.engine);
 app.set("view engine", "handlebars");
 
 app.use(session(sesh));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
-sequelize.sync().then( () => {
-    app.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`);
-    });
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
 });
