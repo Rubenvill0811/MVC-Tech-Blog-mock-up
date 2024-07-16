95d29133229aa461c65d3e7dd664cd8689bf59b9
Rubenvill0811 <rubenvill0811@gmail.com>
2024-07-15 18:37:07 -0700

    commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const handlebars = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config');
+const PORT = process.env.PORT || 3001;
+const cookieStorage = require('connect-session-sequelize')(session.Store);
+const app = express();
+const handlebarsConnection = handlebars.create();
+
+const sesh = {
+    secret: 'hello',
+    cookie: {
+        maxAge: 100000
+    },
+    store: new cookieStorage({db:sequelize})
+}
+app.engine("handlebars", handlebarsConnection.engine);
+app.set("view engine", "handlebars");
+
+app.use(session(sesh));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(routes);
+sequelize.sync().then( () => {
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    });
+});
